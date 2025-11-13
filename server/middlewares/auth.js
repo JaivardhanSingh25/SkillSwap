11c46feb967322bc9a5636a4357ef8a66a825d16
JaivardhanSingh25 <jvardhan2551@gmail.com>
2025-11-13 16:52:10 +0530

    Implemented the authorization middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddle = (req, res) => {
+    const token = req.headers.authorization   // for this to work... you need to set the headers globally as just token not Bearer <token>
+     
+    if (!token) {
+        return res.status(401).json({
+        success: false,
+        message: "No token provided (Unauthorized)",
+    });
+    }
+
+    try{
+        jwt.verify(token, process.env.JWT_SECRET)
+        next()
+    }
+    catch(error){
+        res.status(401).json({
+            success: false,
+            message: 'Token Invalid (Unauthorised)'
+        })
+    }
+}
